9c8b8d38aa1d073c9621c876e74c2dbb8494fcf5
John Green <john-green@github.com>
2024-11-18 22:44:06 -0500

    Add more tests for creating outputs and proofs
    Adds a couple of tests for creating outputs and proofs from the Cashu module. The test vectors are obtained using the nutshell library. See https://gist.github.com/kazluu/2bfee3d4689eb726215949be22303994

--- lib/models/blinded_message.ex
+++ lib/models/blinded_message.ex
@@ -6,35 +6,44 @@ defmodule Cashu.BlindedMessage do
   alias Cashu.{BDHKE, Validator}
   alias Bitcoinex.Secp256k1.Point
 
-  defstruct id: nil, amount: 0, b_prime: nil
+  defstruct id: nil, amount: 0, b_prime: nil, blinding_factor: nil, secret: nil
 
   @type t :: %{
-    id: String.t(),
-    amount: pos_integer(),
-    b_prime: binary()
-  }
+          id: String.t(),
+          amount: pos_integer(),
+          b_prime: binary(),
+          blinding_factor: pos_integer(),
+          secret: binary()
+        }
 
   def new(), do: %__MODULE__{}
   def new(params) when is_list(params), do: struct!(__MODULE__, params)
   def new(params) when is_map(params), do: Map.to_list(params) |> new()
 
-  def new(amount, secret_message, keyset_id)
-      when is_integer(amount) and is_binary(secret_message) do
-    case BDHKE.blind_point(secret_message) do
-      {:ok, blind_point, _blinding_factor} ->
+  def new(amount, secret_message, blinding_factor_hex, keyset_id)
+      when is_integer(amount) and is_binary(secret_message) and is_binary(blinding_factor_hex) do
+    case BDHKE.blind_point(secret_message, blinding_factor_hex) do
+      {:ok, blind_point, blinding_factor} ->
         hex_point = Point.serialize_public_key(blind_point)
-        new(%{amount: amount, id: keyset_id, b_prime: hex_point})
+
+        new(%{
+          amount: amount,
+          id: keyset_id,
+          b_prime: hex_point,
+          blinding_factor: blinding_factor,
+          secret: secret_message
+        })
 
       {:error, _reason} ->
         {:error, "Blind point error"}
     end
   end
 
-  def new(_, _, _), do: {:error, :invalid_params}
+  def new(_, _, _, _), do: {:error, :invalid_params}
 
   def validate(%__MODULE__{amount: amount, id: id, b_prime: b_} = bm) do
-    with {:ok , _} <- Validator.validate_amount(amount),
-         {:ok, _}<- Validator.validate_id(id),
+    with {:ok, _} <- Validator.validate_amount(amount),
+         {:ok, _} <- Validator.validate_id(id),
          {:ok, _} <- Validator.validate_b_(b_) do
       {:ok, bm}
     else
@@ -51,7 +60,7 @@ defimpl Jason.Encoder, for: Cashu.BlindedMessage do
       [
         amount: struct.amount,
         id: struct.id,
-        "B_": struct.b_prime
+        B_: struct.b_prime
       ],
       opts
     )
